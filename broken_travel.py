7c8aecc1d50ac635edfcf1e2387f721889058741
Vedanth_Salil <VS689@live.mdx.ac.uk>
2026-03-07 00:03:45 +0400

    Fixed broken_travel.py

--- broken_travel.py
+++ broken_travel.py
@@ -6,11 +6,12 @@
 # if he is from the distant past (before 1900),
 # the present era (1900-2020) or from the far future (beyond 2020).
 
-year == int.input("Greetings! What is your year of origin? '))
+#removed '==' and changed it to '=' so that year becomes a variable , fixed int bracket and removing '.'
+year = int(input("Greetings! What is your year of origin? "))         #fixed quotations 
 
-if year <= 1900
-    print ('Woah, that's the past!')
-elif year > 1900 && year < 2020:
+if year <= 1900:                                     #added colen to fix  if statement
+    print ("Woah, that's the past!")                 #fixed print statement by changeing ' to "
+elif year >= 1900 and  year <= 2020:                          #fixed and statement
     print ("That's totally the present!")
-elif:
-    print ("Far out, that's the future!!")
+else:                                                  #changed elif to else
+    print ("Far out, that's the future!")
