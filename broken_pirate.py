480430aa20fe75bdea3bef8fc75e5b48dff28ca7
Vedanth_Salil <VS689@live.mdx.ac.uk>
2026-03-06 23:52:03 +0400

    Fixed broken_pirate.py

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,5 @@
-greeting = input("Hello, possible pirate! What's the password?)
+greeting = input("Hello, possible pirate! What's the password?") #Added " so input is fixed
 if greeting in ["Arrr!"]: # NG replaced ) by ]
 	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+else: #changed elif to else , added ':' and put the correct intendation for print statement
+    print("Greetings, hater of pirates!") 
